a3739bd8b215380531f50503d8d0694dd5110235
Kihong <kks4517@naver.com>
2024-01-05 14:25:17 +0900

    conflict: 충돌 수정

--- src/main/java/com/anchor/domain/mentoring/domain/repository/custom/QMentoringApplicationRepository.java
+++ src/main/java/com/anchor/domain/mentoring/domain/repository/custom/QMentoringApplicationRepository.java
@@ -1,10 +1,21 @@
-package com.anchor.domain.mentoring.domain.repository;
+package com.anchor.domain.mentoring.domain.repository.custom;
 
 import com.anchor.domain.mentoring.domain.MentoringApplication;
+import com.anchor.domain.mentoring.domain.MentoringStatus;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface QMentoringApplicationRepository {
+
+  MentoringApplication findByMentoringIdAndProgressTime(
+      Long mentoringId,
+      LocalDateTime startDateTime,
+      LocalDateTime endDateTime
+  );
+
+  List<MentoringApplication> findTimesByMentoringIdAndStatus(
+      Long mentorId, MentoringStatus... statuses);
 
   Optional<MentoringApplication> findAppliedMentoringByTimeAndUserId
       (LocalDateTime startDateTime, LocalDateTime endDateTime, Long userId);
