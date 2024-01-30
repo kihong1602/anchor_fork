7a63bec251e107c90563d1ab9f1b144bfcf19b0b
Kihong <kks4517@naver.com>
2024-01-31 01:18:44 +0900

    feature: 커스텀 예외 정의

--- src/main/java/com/anchor/domain/payment/api/service/PaymentService.java
+++ src/main/java/com/anchor/domain/payment/api/service/PaymentService.java
@@ -4,10 +4,11 @@ import com.anchor.domain.mentoring.domain.MentoringStatus;
 import com.anchor.domain.payment.api.controller.request.PaymentResultInfo;
 import com.anchor.domain.payment.api.service.response.PaymentCompleteResult;
 import com.anchor.domain.payment.domain.repository.PaymentRepository;
-import com.anchor.global.portone.request.RequiredPaymentCreateData;
-import com.anchor.global.portone.response.PaymentResult;
-import com.anchor.global.portone.response.SinglePaymentResult;
-import com.anchor.global.util.PaymentUtils;
+import com.anchor.global.exception.ServiceException;
+import com.anchor.global.payment.portone.request.RequiredPaymentCreateData;
+import com.anchor.global.payment.portone.response.PaymentResult;
+import com.anchor.global.payment.portone.response.SinglePaymentResult;
+import com.anchor.global.util.PaymentClient;
 import com.anchor.global.util.ResponseType;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -20,18 +21,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PaymentService {
 
-  private final PaymentUtils apiUtil;
+  private final PaymentClient paymentClient;
   private final PaymentRepository paymentRepository;
 
   public ResponseType validatePaymentResult(PaymentResultInfo paymentResultInfo) {
     RequiredPaymentCreateData requiredPaymentCreateData = new RequiredPaymentCreateData(paymentResultInfo.getImpUid());
-    Optional<PaymentResult> paymentSelectResult = apiUtil.request(MentoringStatus.WAITING, requiredPaymentCreateData);
-    SinglePaymentResult result = (SinglePaymentResult) paymentSelectResult.orElseThrow(
-        () -> new NoSuchElementException("PaymentSelectResult 값이 존재하지 않습니다."));
-    return ResponseType.of(paymentResultInfo.isSameAs(result));
+    try {
+      Optional<PaymentResult> paymentSelectResult = paymentClient.request(MentoringStatus.WAITING,
+          requiredPaymentCreateData);
+      SinglePaymentResult result = (SinglePaymentResult) paymentSelectResult.orElseThrow(
+          () -> new NoSuchElementException("PaymentSelectResult 값이 존재하지 않습니다."));
+
+      return ResponseType.of(paymentResultInfo.isSameAs(result));
+    } catch (ServiceException e) {
+      log.info(e.getMessage());
+      return ResponseType.FAIL;
+    }
   }
 
   public PaymentCompleteResult getPaymentResult(String orderUid) {
     return paymentRepository.findCompletedPaymentByOrderUid(orderUid);
   }
+
 }
--- src/main/java/com/anchor/domain/user/api/service/UserService.java
+++ src/main/java/com/anchor/domain/user/api/service/UserService.java
@@ -1,5 +1,8 @@
 package com.anchor.domain.user.api.service;
 
+import static com.anchor.global.mail.MentoringMailTitle.CANCEL_BY_MENTEE;
+import static com.anchor.global.mail.MentoringMailTitle.COMPLETE_BY_MENTEE;
+
 import com.anchor.domain.mentor.domain.Mentor;
 import com.anchor.domain.mentoring.api.controller.request.MentoringReviewInfo;
 import com.anchor.domain.mentoring.domain.Mentoring;
@@ -8,6 +11,7 @@ import com.anchor.domain.mentoring.domain.MentoringReview;
 import com.anchor.domain.mentoring.domain.MentoringStatus;
 import com.anchor.domain.mentoring.domain.repository.MentoringApplicationRepository;
 import com.anchor.domain.mentoring.domain.repository.MentoringReviewRepository;
+import com.anchor.domain.notification.domain.ReceiverType;
 import com.anchor.domain.payment.domain.Payment;
 import com.anchor.domain.payment.domain.Payup;
 import com.anchor.domain.payment.domain.repository.PayupRepository;
@@ -22,17 +26,28 @@ import com.anchor.domain.user.api.service.response.UserInfoResponse;
 import com.anchor.domain.user.domain.User;
 import com.anchor.domain.user.domain.repository.UserRepository;
 import com.anchor.global.auth.SessionUser;
-import com.anchor.global.portone.request.RequiredPaymentCancelData;
-import com.anchor.global.portone.response.PaymentCancelResult;
-import com.anchor.global.portone.response.PaymentResult;
-import com.anchor.global.util.PaymentUtils;
+import com.anchor.global.exception.ServiceException;
+import com.anchor.global.exception.type.entity.InvalidStatusException;
+import com.anchor.global.exception.type.entity.MentoringApplicationNotFoundException;
+import com.anchor.global.exception.type.entity.ReviewNotFoundException;
+import com.anchor.global.exception.type.entity.UserNotFoundException;
+import com.anchor.global.mail.AsyncMailSender;
+import com.anchor.global.mail.MailMessage;
+import com.anchor.global.mail.MentoringMailMessage;
+import com.anchor.global.payment.portone.request.RequiredPaymentCancelData;
+import com.anchor.global.payment.portone.response.PaymentCancelResult;
+import com.anchor.global.payment.portone.response.PaymentResult;
+import com.anchor.global.redis.lock.RedisLockFacade;
+import com.anchor.global.redis.message.NotificationEvent;
+import com.anchor.global.util.PaymentClient;
 import com.anchor.global.util.type.DateTimeRange;
+import jakarta.persistence.PersistenceException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -43,12 +58,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
-
   private final UserRepository userRepository;
   private final MentoringApplicationRepository mentoringApplicationRepository;
+  private final ApplicationEventPublisher applicationEventPublisher;
   private final MentoringReviewRepository mentoringReviewRepository;
   private final PayupRepository payupRepository;
-  private final PaymentUtils paymentUtils;
+  private final PaymentClient paymentClient;
+  private final RedisLockFacade redisLockFacade;
+  private final AsyncMailSender asyncMailSender;
 
   @Transactional
   public void writeReview(SessionUser sessionUser, MentoringReviewInfo mentoringReviewInfo) {
@@ -75,7 +92,7 @@ public class UserService {
   @Transactional
   public void editReview(RequiredEditReview editReview) {
     MentoringReview mentoringReview = mentoringReviewRepository.findById(editReview.getId())
-        .orElseThrow(() -> new RuntimeException("리뷰가 존재하지 않습니다."));
+        .orElseThrow(ReviewNotFoundException::new);
     mentoringReview.editReview(editReview);
     mentoringReviewRepository.save(mentoringReview);
   }
@@ -106,7 +123,6 @@ public class UserService {
     userRepository.delete(user);
   }
 
-
   @Transactional(readOnly = true)
   public Page<AppliedMentoringInfo> loadAppliedMentoringList(SessionUser sessionUser, Pageable pageable) {
     User user = getUser(sessionUser);
@@ -117,66 +133,107 @@ public class UserService {
   public boolean changeAppliedMentoringStatus(SessionUser sessionUser, MentoringStatusInfo changeRequest) {
     User user = getUser(sessionUser);
     List<RequiredMentoringStatusInfo> mentoringStatusList = changeRequest.getRequiredMentoringStatusInfos();
-    mentoringStatusList.forEach(status -> {
-      try {
-        validateMentoringStatus(status);
-        changeStatus(user, status);
-      } catch (NoSuchElementException | IllegalArgumentException e) {
-        log.warn(e.getMessage());
-      }
-    });
-
+    List<MailMessage> mailMessages = mentoringStatusList.stream()
+        .map(status -> {
+          MailMessage mailMessage = null;
+          DateTimeRange range = status.getMentoringReservedTime();
+          MentoringApplication mentoringApplication = getMentoringApplication(range.getFrom(), range.getTo(),
+              user.getId());
+          Mentoring mentoring = mentoringApplication.getMentoring();
+          Mentor mentor = mentoring.getMentor();
+          try {
+            validateMentoringStatus(status);
+            changeStatus(mentoringApplication, mentoring, status.getMentoringStatus());
+            mailMessage = createMailMessage(user, mentoring, mentor, range.getFrom(), status.getMentoringStatus());
+          } catch (ServiceException | PersistenceException e) {
+            log.warn("[회원번호 :: {} || 신청내역 번호 :: {}] 상태변경 실패", user.getId(), mentoringApplication.getId());
+          }
+          return mailMessage;
+        })
+        .toList();
+    asyncMailSender.sendMails(mailMessages);
     return true;
+  }
+
+  private MentoringApplication getMentoringApplication(LocalDateTime startDateTime, LocalDateTime endDateTime,
+      Long userId) {
+    return mentoringApplicationRepository.findByStartDateTimeAndEndDateTimeAndUserId(startDateTime, endDateTime, userId)
+        .orElseThrow(MentoringApplicationNotFoundException::new);
+  }
+
+  private MentoringMailMessage createMailMessage(User user, Mentoring mentoring, Mentor mentor,
+      LocalDateTime startDateTime, MentoringStatus status) {
+    String title = switch (status) {
+      case CANCELLED -> CANCEL_BY_MENTEE.getTitle();
+      case COMPLETE -> COMPLETE_BY_MENTEE.getTitle();
+      default -> null;
+    };
+
+    return MailMessage.mentoringMessageBuilder()
+        .title(title)
+        .mentoringTitle(mentoring.getTitle())
+        .receiverEmail(mentor.getCompanyEmail())
+        .opponentEmail(user.getEmail())
+        .opponentNickName(user.getNickname())
+        .startDateTime(startDateTime)
+        .receiverType(ReceiverType.TO_MENTOR)
+        .build();
   }
 
   private void validateMentoringStatus(RequiredMentoringStatusInfo statusInfo) {
     MentoringStatus status = statusInfo.getMentoringStatus();
     if (status.equals(MentoringStatus.WAITING) || status.equals(MentoringStatus.APPROVAL)) {
-      throw new IllegalArgumentException("변경하려는 상태가 'CANCELED' 또는 'COMPLETE'가 아닙니다.");
+      throw new InvalidStatusException();
     }
   }
 
   private User getUser(SessionUser sessionUser) {
     return userRepository.findByEmail(sessionUser.getEmail())
-        .orElseThrow(() -> new NoSuchElementException(sessionUser.getEmail() + "에 해당하는 회원이 존재하지 않습니다."));
+        .orElseThrow(UserNotFoundException::new);
   }
 
   private MentoringApplication getMentoringApplicationByTimeRange(Long userId,
       LocalDateTime startDateTime, LocalDateTime endDateTime) {
-    return mentoringApplicationRepository.findByStartDateTimeAndEndDateTimeAndUserId(
-            startDateTime, endDateTime, userId)
-        .orElseThrow(() -> new RuntimeException("신청내역이 존재하지 않습니다."));
+    return mentoringApplicationRepository.findByStartDateTimeAndEndDateTimeAndUserId(startDateTime, endDateTime, userId)
+        .orElseThrow(MentoringApplicationNotFoundException::new);
   }
 
-  private void changeStatus(User user, RequiredMentoringStatusInfo mentoringStatusInfo) {
-    DateTimeRange dateTimeRange = mentoringStatusInfo.getMentoringReservedTime();
-    MentoringApplication mentoringApplication = getMentoringApplicationByTimeRange(user.getId(),
-        dateTimeRange.getFrom(), dateTimeRange.getTo());
-    mentoringApplication.changeStatus(mentoringStatusInfo.getMentoringStatus());
-    processMentoringStatus(mentoringApplication);
+  private void changeStatus(MentoringApplication mentoringApplication, Mentoring mentoring,
+      MentoringStatus mentoringStatus) {
+    processPayment(mentoringApplication, mentoringStatus);
+    mentoringApplication.changeStatus(mentoringStatus);
+    publishNotification(mentoring, mentoringStatus);
     mentoringApplicationRepository.save(mentoringApplication);
   }
 
-  private void processMentoringStatus(MentoringApplication mentoringApplication) {
-    MentoringStatus mentoringStatus = mentoringApplication.getMentoringStatus();
-    if (mentoringStatus.equals(MentoringStatus.CANCELLED)) {
-      cancelPaymentIfCancelled(mentoringApplication);
-    }
-    if (mentoringStatus.equals(MentoringStatus.COMPLETE)) {
-      savePayup(mentoringApplication);
+  private void publishNotification(Mentoring mentoring, MentoringStatus mentoringStatus) {
+    applicationEventPublisher.publishEvent(NotificationEvent.builder()
+        .email(mentoring.getMentor()
+            .getCompanyEmail())
+        .mentoringId(mentoring.getId())
+        .title(mentoring.getTitle())
+        .mentoringStatus(mentoringStatus)
+        .receiverType(ReceiverType.TO_MENTOR)
+        .build());
+  }
+
+  private void processPayment(MentoringApplication mentoringApplication, MentoringStatus mentoringStatus) {
+    switch (mentoringStatus) {
+      case CANCELLED -> cancelPayment(mentoringApplication, mentoringStatus);
+      case COMPLETE -> savePayup(mentoringApplication);
     }
   }
 
-  private void cancelPaymentIfCancelled(MentoringApplication application) {
-    MentoringStatus status = application.getMentoringStatus();
+  private void cancelPayment(MentoringApplication application, MentoringStatus mentoringStatus) {
     Payment payment = application.getPayment();
     RequiredPaymentCancelData requiredPaymentCancelData = new RequiredPaymentCancelData(payment);
-    Optional<PaymentResult> paymentCancelResult = paymentUtils.request(status, requiredPaymentCancelData);
+    Optional<PaymentResult> paymentCancelResult = paymentClient.request(mentoringStatus, requiredPaymentCancelData);
     paymentCancelResult.ifPresent(result -> payment.editPaymentCancelStatus((PaymentCancelResult) result));
   }
 
   private void savePayup(MentoringApplication application) {
-    Mentoring mentoring = application.getMentoring();
+    Long mentoringId = mentoringApplicationRepository.getMentoringId(application);
+    Mentoring mentoring = redisLockFacade.increaseTotalApplication(mentoringId);
     Mentor mentor = mentoring.getMentor();
     Payment payment = application.getPayment();
     Payup payup = Payup.builder()
@@ -185,4 +242,5 @@ public class UserService {
         .build();
     payupRepository.save(payup);
   }
+
 }
